eb216d9dd4a71c07045d66b103b55157fba5dbcc
Rabby  Lin <rabby@Rabbyde-MacBook-Pro.local>
2023-11-03 22:15:47 +0800

    fix products list / change  HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
