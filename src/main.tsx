0565f56b500f2901dd25e14443d7ae874445e2bc
Rabby <rabby@Rabbyde-MacBook-Pro.local>
2023-11-04 21:56:16 +0800

    add BrowserRouter  basename

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="https://tu22tu22.github.io/tutu-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
